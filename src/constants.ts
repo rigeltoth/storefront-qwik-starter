0d524dc84618c789d3bad09768a8bca56759f286
Ronnovič Rigel Toth <97006094+rigeltoth@users.noreply.github.com>
2025-05-08 14:09:45 -0500

    server connection

--- src/constants.ts
+++ src/constants.ts
@@ -9,11 +9,10 @@ export const HEADER_AUTH_TOKEN_KEY = 'vendure-auth-token';
 export const IMAGE_RESOLUTIONS = [1000, 800, 600, 400];
 export const HOMEPAGE_IMAGE = '/homepage.webp';
 export const DEFAULT_METADATA_URL = 'https://qwik-storefront.vendure.io/';
-export const DEFAULT_METADATA_TITLE = 'Vendure Qwik Storefront';
-export const DEFAULT_METADATA_DESCRIPTION =
-	'A headless commerce storefront starter kit built with Vendure & Qwik';
+export const DEFAULT_METADATA_TITLE = 'RT store';
+export const DEFAULT_METADATA_DESCRIPTION = 'Single-tend store with Vendure & Qwik';
 export const DEFAULT_METADATA_IMAGE = 'https://qwik-storefront.vendure.io/social-image.png';
-export const DEFAULT_LOCALE = 'en';
+export const DEFAULT_LOCALE = 'es';
 export const DEV_API = ENV_VARIABLES.VITE_VENDURE_DEV_URL || 'https://readonlydemo.vendure.io';
-export const PROD_API = ENV_VARIABLES.VITE_VENDURE_PROD_URL || 'https://readonlydemo.vendure.io';
+export const PROD_API = ENV_VARIABLES.VITE_VENDURE_PROD_URL || 'vendure-server-production-1442.up.railway.app';
 export const LOCAL_API = ENV_VARIABLES.VITE_VENDURE_LOCAL_URL || 'http://localhost:3000';
